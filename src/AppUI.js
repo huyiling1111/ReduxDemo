a844282ffba7c3841e332fcb440b5a1ed1b76868
huyiling1111 <1052859577@qq.com>
2021-02-10 17:10:12 +0800

    无状态组件

--- src/AppUI.js
+++ src/AppUI.js
@@ -1,34 +1,36 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd'
-class AppUi extends Component {
-
-    render() {
-        return (
-            <div style={{ margin: '10px' }}>
-                <div>
-                    <Input
-                        style={{ width: '250px', marginRight: '10px' }}
-                        onChange={this.props.changeInputValue}
-                        value={this.props.inputValue}
-                    />
-                    <Button
-                        type="primary"
-                        onClick={this.props.btnAdd}
-                    >增加</Button>
-                </div>
-                <div style={{ margin: '10px', width: '300px' }}>
-                    <List
-                        bordered
-                        dataSource={this.props.list}
-                        renderItem={(item, index) => (<List.Item onClick={() => {
-                            this.props.deleteItem(index)
-                        }}>{item}</List.Item>)}
-                    />
-                </div>
+const AppUi = (props) => {
+    return (
+        <div style={{ margin: '10px' }}>
+            <div>
+                <Input
+                    style={{ width: '250px', marginRight: '10px' }}
+                    onChange={props.changeInputValue}
+                    value={props.inputValue}
+                />
+                <Button
+                    type="primary"
+                    onClick={props.clickBtn}
+                >增加</Button>
             </div>
-        );
-    }
+            <div style={{ margin: '10px', width: '300px' }}>
+                <List
+                    bordered
+                    dataSource={props.list}
+                    renderItem={
+                        (item, index) => (
+                            <List.Item onClick={() => { props.deleteItem(index) }}>
+                                {item}
+                            </List.Item>
+                        )
+                    }
+                />
+            </div>
+        </div>
+    )
+
 }
 
 export default AppUi;
